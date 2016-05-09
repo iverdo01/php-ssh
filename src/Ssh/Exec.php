1ece34e4c375890c379c425f4917f2815911fab2
Ivo Verdonck <iverdo01@jetair.be>
2016-05-09 16:41:14 +0200

    debug without errorcode

--- src/Ssh/Exec.php
+++ src/Ssh/Exec.php
@@ -20,18 +20,12 @@ class Exec extends Subsystem
 
     public function run($cmd, $pty = null, array $env = array(), $width = 80, $height = 25, $width_height_type = SSH2_TERM_UNIT_CHARS)
     {
-        $cmd .= ';echo -ne "[return_code:$?]"';
         $stdout = ssh2_exec($this->getResource(), $cmd, $pty, $env, $width, $height, $width_height_type);
         $stderr = ssh2_fetch_stream($stdout, SSH2_STREAM_STDERR);
         stream_set_blocking($stderr, true);
         stream_set_blocking($stdout, true);
 
         $output = stream_get_contents($stdout);
-        preg_match('/\[return_code:(.*?)\]/', $output, $match);
-        if ((int) $match[1] !== 0) {
-            throw new RuntimeException(stream_get_contents($stderr), (int) $match[1]);
-        }
-
-        return preg_replace('/\[return_code:(.*?)\]/', '', $output);
+	return $output;
     }
 }
